023dd2e1548de722e01a2ea11c9e3f06b2072ffa
dstar55 <dstar55@yahoo.com>
2015-03-21 21:54:07 +0100

    remove unused imports, cleaning the code

--- producer-consumer/producer/src/main/java/com/dockerforjavadevelopers/producer/ProducerConfiguration.java
+++ producer-consumer/producer/src/main/java/com/dockerforjavadevelopers/producer/ProducerConfiguration.java
@@ -1,26 +1,14 @@
 package com.dockerforjavadevelopers.producer;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableAutoConfiguration
 public class ProducerConfiguration {
 
+	/* destination */
     public static String destination = "destination";
-    
-    /*
-    @Autowired
-    private ConfigurableApplicationContext context;
-
-    @Bean
-    JmsTemplate jmsTemplate() {
-        return context.getBean(JmsTemplate.class);
-    }*/
 
 }
